1048a04925dcd2c1b0da9377d6a68e81987f5d4a
vampire16 <siddheshsalvi525@gmai.com>
2020-02-26 16:35:28 +0530

    ADDED : Percentage of singlet combination

--- flipcoin/FlipCoin.sh
+++ flipcoin/FlipCoin.sh
@@ -2,11 +2,31 @@
 
 echo "Flip coin simulation"
 
-#FLIP COIN AND DISPLAY HEADS OR TAIL
-randomCheck=$((RANDOM%2))
-if (( randomCheck == 0 ))
-then
-	echo "HEADS"
-else
-	echo "TAILS"
-fi
+#PERCENTAGE OF SINGLET COMBINATION
+declare -A singletDictionary
+
+#VARIABLES
+H=0
+T=1
+head=0
+tail=0
+singletDictionary[H]=0
+singletDictionary[T]=0
+read -p "Enter how many times you want to flip the coin : " end
+for (( index=1; index<=$end; index++ ))
+do
+	randomCheck=$((RANDOM%2))
+	if (( $randomCheck == $H ))
+	then
+		head=$(($head+1))
+		singletDictionary[H]=$head
+	else
+		tail=$(($tail+1))
+		singletDictionary[T]=$tail
+	fi
+done
+headAvg=`echo "scale = 3 ; (${singletDictionary[H]}*100)/$end" | bc -l` 
+tailAvg=`echo "scale = 3 ; (${singletDictionary[T]}*100)/$end" | bc -l` 
+echo "Percentage of head : $headAvg"
+echo "Percentage of tail : $tailAvg"
+
