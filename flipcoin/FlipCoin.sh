0cafbc714b6372d926fb19faa1b1cb3110758230
vampire16 <siddheshsalvi525@gmai.com>
2020-02-26 14:49:59 +0530

    ADDED : Display flip coin result head or tail

--- flipcoin/FlipCoin.sh
+++ flipcoin/FlipCoin.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Flip coin simulation"
+
+#FLIP COIN AND DISPLAY HEADS OR TAIL
+randomCheck=$((RANDOM%2))
+if (( randomCheck == 0 ))
+then
+	echo "HEADS"
+else
+	echo "TAILS"
+fi
